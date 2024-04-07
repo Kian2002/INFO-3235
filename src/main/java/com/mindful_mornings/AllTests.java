d35f1f98def7e7c465ba59859dcd921c9466887f
Kian2002 <canadakurdistan@gmail.com>
2024-04-07 13:00:07 -0700

    add test suite

---
+++ src/main/java/com/mindful_mornings/AllTests.java
@@ -0,0 +1,13 @@
+package com.mindful_mornings;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AssertTests.class,
+        ParameterizedTests.class
+})
+public class AllTests {
+}
+
