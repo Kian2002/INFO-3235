1e50b6de49629a88801e4bfff80682a16413dac8
Saahib-Dhanda <71110282+Saahib-Dhanda@users.noreply.github.com>
2024-04-10 15:48:24 -0700

    Update AssertTests.java

--- src/main/java/com/mindful_mornings/AssertTests.java
+++ src/main/java/com/mindful_mornings/AssertTests.java
@@ -51,5 +51,45 @@ public class AssertTests {
         assertEquals("Feeling grateful for nature.", app.getGratitudeEntries().get(0));
     }
 
-    // Add more black box tests for Equivalence Partitioning, Boundary Value Analysis, Pairwise Testing
+   //black box testing
+    //Tests Ranges from 6 to 100 if the user enters anything from these ranges it shows invalid 
+    @Test
+    public void testHandleMainMenu_InvalidNumericOption() {
+        ConsoleMindfulMornings app = new ConsoleMindfulMornings();
+        for (int i = 6; i <= 100; i++) { // This tests 6 to 100
+            app.handleMainMenu(String.valueOf(i));
+            assertEquals("For input " + i + ", expected 'Main Menu' to show invalid option handling.", 
+                         "Main Menu", app.getCurrentMenu());
+            }
+        }
+    
+    //black box testing 
+    // made a specific case for the numeral value 0 
+    @Test
+    public void testHandleMainMenu_InvalidNumericOption0() {
+        ConsoleMindfulMornings app = new ConsoleMindfulMornings();
+        app.handleMainMenu("0"); // 
+        assertEquals("Main Menu", app.getCurrentMenu());
+    }
+    
+    //Black Box testing 
+    //If Empty String option is provided invalid 
+    @Test
+    public void testHandleMainMenu_EmptyStringOption() {
+        ConsoleMindfulMornings app = new ConsoleMindfulMornings();
+        app.handleMainMenu("");
+        assertEquals("Main Menu", app.getCurrentMenu());
+    }
+    
+    
+    //Black Box Testing 
+    //If negative number's entered ranging from -1 to -100 Prompt Invalid Option 
+    public void testHandleMainMenu_InvalidNegativeNumericOption() {
+        ConsoleMindfulMornings app = new ConsoleMindfulMornings();
+        for (int i = -1; i <= -100; i++) { // This tests -1 to -100 
+            app.handleMainMenu(String.valueOf(i));
+            assertEquals("For input " + i + ", expected 'Main Menu' to show invalid option handling.", 
+                         "Main Menu", app.getCurrentMenu());
+            }
+        }
 }
